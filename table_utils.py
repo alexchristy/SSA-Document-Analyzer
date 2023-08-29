6ec6bf3909c8726fdaa1dfca1d87684828993c76
Alex Christy <a.christy@ufl.edu>
2023-08-29 11:32:25 -0400

    More robust error handling for convert json to tables

--- table_utils.py
+++ table_utils.py
@@ -6,13 +6,13 @@ def convert_textract_response_to_tables(json_response):
     """Convert AWS Textract JSON response to a list of Table objects."""
     try:
         tables = []
-        block_id_to_block = {block['Id']: block for block in json_response.get('Blocks', [])}
+        block_id_to_block = {block['Id']: block for block in json_response.get('Blocks', []) if 'Id' in block}
 
         # Helper function to collect text from child blocks
         def collect_text_from_children(block):
             text = block.get('Text', '')
             for relationship in block.get('Relationships', []):
-                if relationship['Type'] == 'CHILD':
+                if relationship.get('Type') == 'CHILD':
                     for child_id in relationship.get('Ids', []):
                         child_block = block_id_to_block.get(child_id, {})
                         text += ' ' + collect_text_from_children(child_block)
@@ -21,14 +21,20 @@ def convert_textract_response_to_tables(json_response):
         current_table = None
 
         for block in json_response.get('Blocks', []):
-            if block.get('BlockType') == 'TABLE':
+            block_type = block.get('BlockType')
+            
+            if block_type == 'TABLE':
                 current_table = Table()
                 current_table.table_number = len(tables) + 1
                 current_table.table_confidence = block.get('Confidence', 0.0)
                 current_table.page_number = block.get('Page', 1)  # Setting the page number
                 tables.append(current_table)
                 
-            elif block.get('BlockType') == 'CELL':
+            elif block_type == 'CELL':
+                if current_table is None:
+                    logging.warning("Encountered a CELL block before a TABLE block.")
+                    continue
+                
                 cell_text = collect_text_from_children(block)
                 cell_confidence = block.get('Confidence', 0.0)
                 row_index = block.get('RowIndex', 0) - 1
@@ -39,17 +45,19 @@ def convert_textract_response_to_tables(json_response):
                 current_row = current_table.rows[row_index]
                 current_row.append((cell_text, cell_confidence))
 
-            elif block.get('BlockType') == 'TABLE_TITLE':
-                title_text = collect_text_from_children(block)
-                current_table.title = title_text
-                current_table.title_confidence = block.get('Confidence', 0.0)
+            elif block_type == 'TABLE_TITLE':
+                if current_table:
+                    title_text = collect_text_from_children(block)
+                    current_table.title = title_text
+                    current_table.title_confidence = block.get('Confidence', 0.0)
 
-            elif block.get('BlockType') == 'TABLE_FOOTER':
-                footer_text = collect_text_from_children(block)
-                current_table.footer = footer_text
-                current_table.footer_confidence = block.get('Confidence', 0.0)
+            elif block_type == 'TABLE_FOOTER':
+                if current_table:
+                    footer_text = collect_text_from_children(block)
+                    current_table.footer = footer_text
+                    current_table.footer_confidence = block.get('Confidence', 0.0)
                     
-        return tables
+        return tables if tables else None
     except Exception as e:
         logging.error(f"An error occurred while converting to table: {e}")
         return None
