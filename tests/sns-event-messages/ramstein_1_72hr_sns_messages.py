dec66812bcc1d6894a09ef23f50ddb9109051a4e
Alex Christy <a.christy@ufl.edu>
2023-09-02 10:55:41 -0400

    Add textract and sns and pdf for ramstein_1_72hr

---
+++ tests/sns-event-messages/ramstein_1_72hr_sns_messages.py
@@ -0,0 +1,22 @@
+ramstein_1_72hr_successful_job_sns_message = {
+    "Records": [
+        {
+            "EventSource": "aws:sns",
+            "EventVersion": "1.0",
+            "EventSubscriptionArn": "arn:aws:sns:us-east-2:761356229218:Textract-PDF-Flight-Data-Job-Results:7b8ea79b-8065-4587-8303-3128664d7615",
+            "Sns": {
+                "Type": "Notification",
+                "MessageId": "aa5f4c28-b36e-59b2-89c0-114584cd53ba",
+                "TopicArn": "arn:aws:sns:us-east-2:761356229218:Textract-PDF-Flight-Data-Job-Results",
+                "Subject": 'null',
+                "Message": "{\"JobId\":\"c7dba6b03949e6a691d9904ccf0f8e36b6f71fc2b3b37707e9569a7dd2d6dc6d\",\"Status\":\"SUCCEEDED\",\"API\":\"StartDocumentAnalysis\",\"Timestamp\":1693660515923,\"DocumentLocation\":{\"S3ObjectName\":\"current/72_HR/72 Hr - Ramstein AFPIMS_fc50d78f-1.pdf\",\"S3Bucket\":\"testing-ssa-pdf-store\"}}",
+                "Timestamp": "2023-09-02T13:15:15.973Z",
+                "SignatureVersion": "1",
+                "Signature": "ou0+5dDd1+vu6JPjsz7vly9OaHQPrQWRgARnvVmZuyMizagMssjHkjw7XrJXBSmG4Tt+Tggdo+cX4xoQILKV/Kt0FoQkOEAARfA6yGtmslq/TIL6Mi/eB5jKst3SxGaCUS5PtiwE5sSgeJ4k5SXnJ4gPien09eePsBAhlj86X70lp/8uhfyyPUm4A9OTqq1iD5QUDeLTkoZgAvviiBXJaB9iMBfjrzdInF/T/ZU/dDczpozpo3nykw2CXj3dVtzo8PJ8p3z7rGiGdcInMY+JpaKhE5c56nQBFgNwA4+0TE7Sg2HFVbqTzLnR+wSzAOtwT9dN8KQFBE0lFW70OZyYyQ==",
+                "SigningCertUrl": "https://sns.us-east-2.amazonaws.com/SimpleNotificationService-01d088a6f77103d0fe307c0069e40ed6.pem",
+                "UnsubscribeUrl": "https://sns.us-east-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-2:761356229218:Textract-PDF-Flight-Data-Job-Results:7b8ea79b-8065-4587-8303-3128664d7615",
+                "MessageAttributes": {}
+            }
+        }
+    ]
+}
