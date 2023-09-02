60d097ef4040836342bbf8df6116d3258236f69d
Alex Christy <a.christy@ufl.edu>
2023-09-02 08:54:06 -0400

    Add iwakuni_1_72hr test information

---
+++ tests/sns-event-messages/iwakuni_1_72hr_sns_messages.py
@@ -0,0 +1,22 @@
+iwakuni_1_72hr_successful_job_sns_message = {
+    "Records": [
+        {
+            "EventSource": "aws:sns",
+            "EventVersion": "1.0",
+            "EventSubscriptionArn": "arn:aws:sns:us-east-2:761356229218:Textract-PDF-Flight-Data-Job-Results:7b8ea79b-8065-4587-8303-3128664d7615",
+            "Sns": {
+                "Type": "Notification",
+                "MessageId": "bab1f306-901e-54a7-92c7-ae6ba25bd92f",
+                "TopicArn": "arn:aws:sns:us-east-2:761356229218:Textract-PDF-Flight-Data-Job-Results",
+                "Subject": 'null',
+                "Message": "{\"JobId\":\"e2138ddc76d780d2e13bbd98a344d59315143e86d3b54abafa5a6769eb6a792d\",\"Status\":\"SUCCEEDED\",\"API\":\"StartDocumentAnalysis\",\"Timestamp\":1693659141915,\"DocumentLocation\":{\"S3ObjectName\":\"current/72_HR/72 Hour Slides AUG 18_fd040263-b.pdf\",\"S3Bucket\":\"testing-ssa-pdf-store\"}}",
+                "Timestamp": "2023-09-02T12:52:21.968Z",
+                "SignatureVersion": "1",
+                "Signature": "Rwjg0P1JLCl/L94vgn01dT+jmvJBl6OMfgVboQIAPd4lNsCtWtpr6s+t+0pB1fj+prwtab9/l8MEN49lAn7s7a9ZVj6Rb/5Z2qkN27fN3uONCG1k7brhshP9IyDmOrkQ2EObNA0Pb4YytNPIFkfXaO+LJbyUAuFVYR8uYWeH/RiEdB52C69ntDWe9skRcwK1uzaBN0nwrEfeljAzKucijwrY3Le5tDmbfwZ2D4KO/1XKXIAIEmgslawh84kqRo77dIn+Pacxj2dJRmTpWsNqhRmMgf8E+x+wS1+k19PpVOCQAFfu7+5Tg9isYz63lINaJeZzWnlvN1FFzNLvwVXZuA==",
+                "SigningCertUrl": "https://sns.us-east-2.amazonaws.com/SimpleNotificationService-01d088a6f77103d0fe307c0069e40ed6.pem",
+                "UnsubscribeUrl": "https://sns.us-east-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-2:761356229218:Textract-PDF-Flight-Data-Job-Results:7b8ea79b-8065-4587-8303-3128664d7615",
+                "MessageAttributes": {}
+            }
+        }
+    ]
+}
