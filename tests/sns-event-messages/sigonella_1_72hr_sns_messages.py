46069bfcb5f0606b5c8c85e9c404f7bbfc26e8df
Alex Christy <a.christy@ufl.edu>
2023-09-05 09:18:35 -0400

    Add sigonella_1_72hr data ONLY for testing

---
+++ tests/sns-event-messages/sigonella_1_72hr_sns_messages.py
@@ -0,0 +1,22 @@
+sigonella_1_72hr_successful_job_sns_message = {
+    "Records": [
+        {
+            "EventSource": "aws:sns",
+            "EventVersion": "1.0",
+            "EventSubscriptionArn": "arn:aws:sns:us-east-2:761356229218:Textract-PDF-Flight-Data-Job-Results:7b8ea79b-8065-4587-8303-3128664d7615",
+            "Sns": {
+                "Type": "Notification",
+                "MessageId": "e3d81e34-4273-576c-a1c3-435bb5340985",
+                "TopicArn": "arn:aws:sns:us-east-2:761356229218:Textract-PDF-Flight-Data-Job-Results",
+                "Subject": 'null',
+                "Message": "{\"JobId\":\"7409c01f57ac7054b80ed8768180d62bee9f22190532c4e72e2df3a8d2eb500a\",\"Status\":\"SUCCEEDED\",\"API\":\"StartDocumentAnalysis\",\"Timestamp\":1693840647648,\"DocumentLocation\":{\"S3ObjectName\":\"current/72_HR/SIZ_72HR_18_AUG 23 REV_87f3086f-f.pdf\",\"S3Bucket\":\"testing-ssa-pdf-store\"}}",
+                "Timestamp": "2023-09-04T15:17:27.694Z",
+                "SignatureVersion": "1",
+                "Signature": "n4TvLDT78NbpSSoTw1Eee5Z/AgdSACqvNWxAEPk/rWsRIu/5Yvz1sPBOlDOte8XxeqOiTa5lNm07jj3P8f7K6hZFgzyqruzlNuFA4p0dXn2gZb5snhj51J+j1geS9E3Eyxk2OSjKJpZUOFdHyKc4mmCBi0IyLfeYl0NTcKi4MgFnbLxKDFJvwfzs5/RQHNd2nvsaXnkpzXU8RPLbf0STbQlwjZ6axaE2mnskgq0buraj7FsS+3WPUBVdFFNsdAX61f9hW8TfX7Ga3bcijFtv/lVM8ik9JZStcIb1U0s0IKGBKriTDOlYW/Av/RgyRj4Qy8iGEGI9vtxJVx0iZ3ffDg==",
+                "SigningCertUrl": "https://sns.us-east-2.amazonaws.com/SimpleNotificationService-01d088a6f77103d0fe307c0069e40ed6.pem",
+                "UnsubscribeUrl": "https://sns.us-east-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-2:761356229218:Textract-PDF-Flight-Data-Job-Results:7b8ea79b-8065-4587-8303-3128664d7615",
+                "MessageAttributes": {}
+            }
+        }
+    ]
+}
