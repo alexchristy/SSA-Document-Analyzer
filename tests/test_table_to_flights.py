711856286b1c5eb96520a4c519c7df46f5db76b9
Alex Christy <a.christy@ufl.edu>
2023-09-10 11:47:13 -0400

    Updated al_udeid_1_72hr table to flight test to run conversion in parallel

--- tests/test_table_to_flights.py
+++ tests/test_table_to_flights.py
@@ -1,5 +1,7 @@
 import unittest
+from concurrent.futures import ThreadPoolExecutor
 import sys
+import time
 
 # For Table class
 sys.path.append("..")
@@ -10,7 +12,9 @@ from flight_utils import convert_72hr_table_to_flights
 class TestTableToFlights(unittest.TestCase):
     
     def test_al_udeid_1_72hr(self):
-        
+
+        origin_terminal = 'Al Udeid AB Passenger Terminal'
+
         # Load tables
         table1 = Table.load_state("tests/table-objects/al_udeid_1_72hr_table-1.pkl")
         table2 = Table.load_state("tests/table-objects/al_udeid_1_72hr_table-2.pkl")
@@ -43,29 +47,34 @@ class TestTableToFlights(unittest.TestCase):
         table3_flights.append(Flight.load_state("tests/flight-objects/al_udeid_1_72hr_table-3_flight-5.pkl"))
         table3_flights.append(Flight.load_state("tests/flight-objects/al_udeid_1_72hr_table-3_flight-6.pkl"))
 
-        origin_terminal = 'Al Udeid AB Passenger Terminal'
+        # Use ThreadPoolExecutor to run conversions in parallel
+        with ThreadPoolExecutor() as executor:
+            futures = {
+                'table1': executor.submit(convert_72hr_table_to_flights, table1, origin_terminal),
+                'table2': executor.submit(convert_72hr_table_to_flights, table2, origin_terminal),
+                'table3': executor.submit(convert_72hr_table_to_flights, table3, origin_terminal)
+            }
 
-        # Convert tables to flights
-        table1_flights = convert_72hr_table_to_flights(table1, origin_terminal)
-        table2_flights = convert_72hr_table_to_flights(table2, origin_terminal)
-        table3_flights = convert_72hr_table_to_flights(table3, origin_terminal)
+            table1_converted_flights = futures['table1'].result()
+            table2_converted_flights = futures['table2'].result()
+            table3_converted_flights = futures['table3'].result()
 
         # Check that the flights are the same
-        self.assertEqual(len(table1_flights), len(table1_flights))
-        self.assertEqual(len(table2_flights), len(table2_flights))
-        self.assertEqual(len(table3_flights), len(table3_flights))
+        self.assertEqual(len(table1_converted_flights), len(table1_flights))
+        self.assertEqual(len(table2_converted_flights), len(table2_flights))
+        self.assertEqual(len(table3_converted_flights), len(table3_flights))
 
         # Check that the converted flights are the same as the known good flights:
         # Table 1
-        for i, flight in enumerate(table1_flights):
+        for i, flight in enumerate(table1_converted_flights):
             self.assertEqual(flight, table1_flights[i])
 
         # Table 2
-        for i, flight in enumerate(table2_flights):
+        for i, flight in enumerate(table2_converted_flights):
             self.assertEqual(flight, table2_flights[i])
 
         # Table 3
-        for i, flight in enumerate(table3_flights):
+        for i, flight in enumerate(table3_converted_flights):
             self.assertEqual(flight, table3_flights[i])
 
     def test_andersen_1_72hr(self):
