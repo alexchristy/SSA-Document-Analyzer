6315adc559656d8ea464ccf0ea51d88987551981
Alex Christy <a.christy@ufl.edu>
2023-09-17 17:47:27 -0400

    Update flight class to have patriot express bool and destination note bool

--- flight.py
+++ flight.py
@@ -4,7 +4,7 @@ import pickle
 
 class Flight:
 
-    def __init__(self, origin_terminal: str, destinations: list, rollcall_time: str, num_of_seats: int, seat_status: str, notes: str, date: str, rollcall_note=False, seat_note=False):
+    def __init__(self, origin_terminal: str, destinations: list, rollcall_time: str, num_of_seats: int, seat_status: str, notes: str, date: str, rollcall_note=False, seat_note=False, destination_note=False, patriot_express=False):
 
         self.origin_terminal = origin_terminal
         self.destinations = destinations
@@ -15,6 +15,8 @@ class Flight:
         self.date = date
         self.rollcall_note = rollcall_note
         self.seat_note = seat_note
+        self.destination_note = destination_note
+        self.patriot_express = patriot_express
 
         # Generate a deterministic unique flight ID based on attributes using SHA-256
         attributes_str = f"{self.origin_terminal}{self.destinations}{self.rollcall_time}{self.num_of_seats}{self.seat_status}{self.notes}{self.date}"
@@ -35,9 +37,11 @@ class Flight:
             'flight_id': self.flight_id,
             'date': self.date,
             'rollcall_note': self.rollcall_note,
-            'seat_note': self.seat_note
+            'seat_note': self.seat_note,
+            'destination_note': self.destination_note,
+            'patriot_express': self.patriot_express
         }
-    
+
     def pretty_print(self):
         print(f"{'=' * 40}")
         print(f"Flight ID: {self.flight_id}")
@@ -46,11 +50,12 @@ class Flight:
         print(f"Destination/s: {self.destinations if self.destinations else 'N/A'}")
         rollcall_text = "**See note below**" if self.rollcall_note else self.rollcall_time
         print(f"Roll Call Time: {rollcall_text if rollcall_text else 'N/A'}")
-        seat_status_text = "**See note below**" if self.seat_note else self.seat_status  # Added this line
+        seat_status_text = "**See note below**" if self.seat_note else self.seat_status
         print(f"Number of Seats: {self.num_of_seats if self.num_of_seats is not None else 0}")
-        print(f"Seat Status: {seat_status_text if seat_status_text else 'N/A'}")  # Updated this line
+        print(f"Seat Status: {seat_status_text if seat_status_text else 'N/A'}")
         print(f"Notes: {self.notes if self.notes else 'N/A'}")
         print(f"Date: {self.date if self.date else 'N/A'}")
+        print(f"Patriot Express: {self.patriot_express}")
         print(f"{'=' * 40}")
 
     def __eq__(self, other):
@@ -65,8 +70,10 @@ class Flight:
             self.notes == other.notes and
             self.date == other.date and
             self.flight_id == other.flight_id and
-            self.rollcall_note == other.rollcall_note and  # Corrected this line
-            self.seat_note == other.seat_note  # Added this line
+            self.rollcall_note == other.rollcall_note and
+            self.seat_note == other.seat_note and
+            self.destination_note == other.destination_note and
+            self.patriot_express == other.patriot_express
         )
 
     
