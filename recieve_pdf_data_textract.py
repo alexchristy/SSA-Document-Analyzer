e0af3e9af10e3a3bdce7fdbdf96ab5ec14b223f5
Alex Christy <a.christy@ufl.edu>
2023-10-11 18:47:53 -0400

    Added norfolk_1_72hr test table to flights

--- recieve_pdf_data_textract.py
+++ recieve_pdf_data_textract.py
@@ -20,8 +20,8 @@ import sys
 sys.path.append("./tests/textract-responses")
 sys.path.append("./tests/sns-event-messages")
 
-from mildenhall_1_72hr_sns_messages import mildenhall_1_72hr_successful_job_sns_message as current_sns_message
-from mildenhall_1_72hr_textract_response import mildenhall_1_72hr_textract_response as current_textract_response
+from norfolk_1_72hr_sns_messages import norfolk_1_72hr_successful_job_sns_message as current_sns_message
+from norfolk_1_72hr_textract_response import norfolk_1_72hr_textract_response as current_textract_response
 
 def initialize_clients():
     # Set environment variables
@@ -292,7 +292,7 @@ def lambda_handler(event, context):
     #     Table.save_state(table, f'table-{idx}.pkl')
     # print(hashlib.sha256(table_str.encode()).hexdigest())
         
-    table_pkl_path = 'tests/table-objects/mildenhall_1_72hr_table-4.pkl'
+    table_pkl_path = 'tests/table-objects/norfolk_1_72hr_table-3.pkl'
 
     custom_date = '20230910'
 
