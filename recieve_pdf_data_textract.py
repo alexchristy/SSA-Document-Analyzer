0406dae003130cf33c342cee6cef8ffbf1d09047
Alex Christy <a.christy@ufl.edu>
2023-10-12 08:25:36 -0400

    Added seattle_1_72hr test table to flights

--- recieve_pdf_data_textract.py
+++ recieve_pdf_data_textract.py
@@ -20,8 +20,8 @@ import sys
 sys.path.append("./tests/textract-responses")
 sys.path.append("./tests/sns-event-messages")
 
-from scott_1_72hr_sns_messages import scott_1_72hr_successful_job_sns_message as current_sns_message
-from scott_1_72hr_textract_response import scott_1_72hr_textract_response as current_textract_response
+from seattle_1_72hr_sns_messages import seattle_1_72hr_successful_job_sns_message as current_sns_message
+from seattle_1_72hr_textract_response import seattle_1_72hr_textract_response as current_textract_response
 
 def initialize_clients():
     # Set environment variables
@@ -292,7 +292,7 @@ def lambda_handler(event, context):
     #     Table.save_state(table, f'table-{idx}.pkl')
     # print(hashlib.sha256(table_str.encode()).hexdigest())
         
-    table_pkl_path = 'tests/table-objects/scott_1_72hr_table-2.pkl'
+    table_pkl_path = 'tests/table-objects/seattle_1_72hr_table-3.pkl'
 
     custom_date = '20230910'
 
