56edb9ce57256a73f749a8bcf26cd59ef11dbc78
Alex Christy <a.christy@ufl.edu>
2023-11-07 10:16:28 -0500

    Added incirlik_1_72hr e2e test

--- tests/test_pdf_to_flights_e2e.py
+++ tests/test_pdf_to_flights_e2e.py
@@ -1044,8 +1044,7 @@ class TestPdfToFlightsE2E(unittest.TestCase):
         """Test that a valid 72hr pdf with no flights is logged and able to be caught in Firestore.
 
         Because this this is a valid 72 hour PDF and has empty tables it should make it all the way to
-        the Process-72hr-Flights Lambda. This Lambda will then raise an alarm in Cloudwatch as it processes
-        the PDF and finds no flights.
+        the Process-72hr-Flights Lambda.
         """
         s3_client = S3Bucket(bucket_name="testing-ssa-pdf-store")
         fs = FirestoreClient()
@@ -1266,3 +1265,55 @@ class TestPdfToFlightsE2E(unittest.TestCase):
 
         for i, flight in enumerate(test_flights):
             self.assertEqual(flight, good_flights[i])
+
+    def test_flight_convert_incirlik_1_72hr(self: unittest.TestCase) -> None:
+        """Test that a valid 72hr pdf with no flights is logged and able to be caught in Firestore.
+
+        Because this this is a valid 72 hour PDF and has empty tables it should make it all the way to
+        the Process-72hr-Flights Lambda.
+        """
+        s3_client = S3Bucket(bucket_name="testing-ssa-pdf-store")
+        fs = FirestoreClient()
+
+        pdf_hash = "f16e10fa71e129b41d2f751d35e419c6082e4c522b18de0bdf4aedb4e13dccfc"
+
+        pdf_doc = {
+            "cloud_path": "current/72_HR/incirlik_1_72hr_test.pdf",
+            "hash": pdf_hash,
+            "terminal": "Incirlik AB Passenger Terminal",
+            "type": "72_HR",
+        }
+
+        fs.insert_document_with_id(
+            collection_name="**TESTING**_PDF_Archive",
+            document_data=pdf_doc,
+            doc_id=pdf_hash,
+        )
+
+        s3_client.upload_to_s3(
+            local_path="tests/end-to-end-test-assets/test_flight_convert_incirlik_1_72hr/incirlik_1_72hr_test.pdf",
+            s3_path="current/72_HR/incirlik_1_72hr_test.pdf",
+        )
+
+        # Wait for job to finish
+        retries = 10
+        while True:
+            time.sleep(60)
+            retries -= 1
+            textract_jobs = fs.get_all_failed_proc_72_flights(
+                lookback_seconds=3600, buffer_seconds=360
+            )
+
+            if not textract_jobs:
+                if retries <= 0:
+                    self.fail("No failed jobs found after 10 retries.")
+                continue
+
+            for job in textract_jobs:
+                if job.get("pdf_hash", None) == pdf_hash:
+                    # Delete job from Firestore
+                    fs.delete_document_by_id("Textract_Jobs", job["job_id"])
+
+                    self.assertEqual(job["finished_72hr_processing"], None)
+                    break
+            break
