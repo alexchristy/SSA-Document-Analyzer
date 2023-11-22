a8a7744d8dec1412821c8eb411d510581108f212
Alex Christy <a.christy@ufl.edu>
2023-11-22 15:04:05 -0500

    Add test flag in Process-72HR-Flights

--- process_72hr_flights.py
+++ process_72hr_flights.py
@@ -26,20 +26,9 @@ sentry_sdk.init(
     profiles_sample_rate=1.0,
 )
 
-MIN_CONFIDENCE = 80
-
-lambda_client = initialize_client("lambda")
-textract_client = initialize_client("textract")
-
-# Initialize Firestore client
-firestore_client = FirestoreClient()
-
 # Initialize logger and set log level
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-# Load store_flights_in_firestore Lambda function name from environment variable
-STORE_FLIGHTS_LAMBDA = os.getenv("STORE_FLIGHTS_LAMBDA")
 
 
 def array_to_dict(array: List[Any]) -> dict:
@@ -77,14 +66,83 @@ def lambda_handler(event: dict, context: lambda_context.Context) -> Dict[str, An
         dict: A dictionary containing the response from the Lambda function.
     """
     try:
+        lambda_client = initialize_client("lambda")
+
+        # Initialize Firestore client with a default textract job collection
+        firestore_client = FirestoreClient(textract_jobs_coll="Textract_Jobs")
+
         logging.info("Event: %s", event)
         # Get tables from event, if any
         event_tables = event.get("tables", [])
         pdf_hash = event.get("pdf_hash", "")
         job_id = event.get("job_id", "")
 
+        if not event_tables:
+            response_msg = f"No tables found in payload: {event}"
+            raise ValueError(response_msg)
+
+        if not pdf_hash:
+            response_msg = f"No pdf_hash found in payload: {event}"
+            raise ValueError(response_msg)
+
+        if not job_id:
+            response_msg = f"No job_id found in payload: {event}"
+            raise ValueError(response_msg)
+
         logging.info("PDF Hash: %s", pdf_hash)
         logging.info("Job ID: %s", job_id)
+
+        # Check for test parameters
+        textract_doc = firestore_client.get_textract_job(job_id)
+
+        test = False
+        use_test_date = False
+        test_date = None
+        if textract_doc:
+            test = textract_doc.get("test", False)
+
+            pdf_archive_coll = None
+            terminal_coll = None
+
+            if test:
+                logging.info("Using test values.")
+                test_params: Dict[str, Any] = textract_doc.get("testParameters", {})
+
+                if not test_params:
+                    msg = "Test parameters not found"
+                    raise ValueError(msg)
+
+                # Set testing terminal and pdf collections
+                if "testPdfArchiveColl" in test_params:
+                    pdf_archive_coll = test_params["testPdfArchiveColl"]
+
+                    if isinstance(pdf_archive_coll, str):
+                        firestore_client.set_pdf_archive_coll(pdf_archive_coll)
+                else:
+                    logging.error("Failed to get test pdf archive collection.")
+
+                if "testTerminalColl" in test_params:
+                    terminal_coll = test_params["testTerminalColl"]
+
+                    if isinstance(terminal_coll, str):
+                        firestore_client.set_terminal_coll(terminal_coll)
+                else:
+                    logging.error("Failed to get test terminal collection.")
+
+                valid_date_time_length = 12
+                if (
+                    "testDateTime" in test_params
+                    and len(test_params["testDateTime"]) == valid_date_time_length
+                ):
+                    time_data = test_params["testDateTime"]
+                    use_test_date = True
+                    test_date = time_data[0:8]
+                else:
+                    logging.error("Failed to get testDateTime.")
+        else:
+            logging.error(
+                "Failed to get textract job document when checking for testing values."
+            )
 
         request_id = context.aws_request_id
         function_name = context.function_name
@@ -121,28 +179,24 @@ def lambda_handler(event: dict, context: lambda_context.Context) -> Dict[str, An
             response_msg = f"No tables found in payload: {event}"
             raise ValueError(response_msg)
 
-        if not pdf_hash:
-            response_msg = f"No pdf_hash found in payload: {event}"
-            raise ValueError(response_msg)
-
-        if not job_id:
-            response_msg = f"No job_id found in payload: {event}"
-            raise ValueError(response_msg)
-
         firestore_client.add_job_timestamp(job_id, "started_72hr_processing")
 
         # Get the origin terminal from Firestore
         origin_terminal = firestore_client.get_terminal_name_by_pdf_hash(pdf_hash)
 
         if not origin_terminal:
-            msg = f"Could not retrieve terminal name for pdf_hash: {pdf_hash}"
+            msg = f"Could not retrieve terminal name for pdf_hash: {pdf_hash}. Searching in {firestore_client.pdf_archive_coll} collection."
             raise ValueError(msg)
 
         flights: List[Flight] = []
         for i, table in enumerate(tables):
             # Create flight objects from table
+            # Fixed data is used for testing if test flag is set and testDateTime is present
             curr_flights = convert_72hr_table_to_flights(
-                table, origin_terminal=origin_terminal
+                table=table,
+                origin_terminal=origin_terminal,
+                use_fixed_date=use_test_date,
+                fixed_date=test_date,
             )
 
             if curr_flights:
@@ -182,9 +236,12 @@ def lambda_handler(event: dict, context: lambda_context.Context) -> Dict[str, An
             "terminal": origin_terminal,
         }
 
+        # Load store_flights_in_firestore Lambda function name from environment variable
+        store_flights_lambda = os.getenv("STORE_FLIGHTS_LAMBDA")
+
         # Invoke store_flights Lambda function
         lambda_client.invoke(
-            FunctionName=STORE_FLIGHTS_LAMBDA,
+            FunctionName=store_flights_lambda,
             InvocationType="Event",
             Payload=json.dumps(payload),
         )
