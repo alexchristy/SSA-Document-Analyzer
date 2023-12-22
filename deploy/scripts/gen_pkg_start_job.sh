ef2f290523e6255ca97c0f3131600cb7be65e2e6
alexchristy <a.christy@ufl.edu>
2023-12-22 15:12:32 -0500

    Updated script to include google-cloud-firestore

--- deploy/scripts/gen_pkg_start_job.sh
+++ deploy/scripts/gen_pkg_start_job.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
-rm -rf start_job*
-mkdir start_job
-cd start_job
-source ../../../start_job_env/bin/activate
-pip install -r ../../../dependencies/start_job_requirements.txt --target .
-cp ../../../*.py ./
-cp ../../../creds.json ./
+
+# Define the root directory for the lambda function
+LAMBDA_ROOT_DIR="start_job"
+
+# Clear and recreate the lambda root directory
+rm -rf $LAMBDA_ROOT_DIR
+mkdir $LAMBDA_ROOT_DIR
+
+# Install dependencies in the lambda root directory
+pip install -r ../../dependencies/start_job_requirements.txt --target $LAMBDA_ROOT_DIR
+
+# Copy Python files and credentials to the lambda root directory
+cp ../../*.py $LAMBDA_ROOT_DIR/
+cp ../../creds.json $LAMBDA_ROOT_DIR/
+
+# Create the deployment package
+cd $LAMBDA_ROOT_DIR
 zip -r9 ../start_job.zip .
+
+# Clean up
 cd ..
-rm -rf start_job
+rm -rf $LAMBDA_ROOT_DIR
