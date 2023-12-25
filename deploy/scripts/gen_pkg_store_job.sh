0aa76cb0fd7d8cccef4fd5fb625a0c8887ec546f
alexchristy <a.christy@ufl.edu>
2023-12-25 17:25:28 -0500

    Refactor deployment scripts to use a common root directory

--- deploy/scripts/gen_pkg_store_job.sh
+++ deploy/scripts/gen_pkg_store_job.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-rm -rf store_flights*
-mkdir store_flights
-cd store_flights
-source ../../../store_flights_env/bin/activate
-pip install -r ../../../dependencies/store_flights_requirements.txt --target .
-cp ../../../*.py ./
-cp ../../../creds.json ./
+
+# Define the root directory for the lambda function
+LAMBDA_ROOT_DIR="store_flights"
+
+# Clear and recreate the lambda root directory
+rm -rf $LAMBDA_ROOT_DIR
+mkdir $LAMBDA_ROOT_DIR
+
+# Install dependencies in the lambda root directory
+# Note: The virtual environment activation is not necessary
+pip install -r ../../dependencies/store_flights_requirements.txt --target $LAMBDA_ROOT_DIR
+
+# Copy Python files and credentials to the lambda root directory
+cp ../../*.py $LAMBDA_ROOT_DIR/
+cp ../../creds.json $LAMBDA_ROOT_DIR/
+
+# Create the deployment package
+cd $LAMBDA_ROOT_DIR
 zip -r9 ../store_flights.zip .
+
+# Clean up
 cd ..
-rm -rf store_flights
+rm -rf $LAMBDA_ROOT_DIR
